3f58e7be28457fb7403d36e75528e7956cb538df
FrancoisRem <44405815+FrancoisRem@users.noreply.github.com>
2018-10-24 11:54:04 +0200

    Add files via upload

---
+++ SpaceInvaders/SpaceInvaders.v
@@ -0,0 +1,15 @@
+module SpaceInvaders (
+  input clk,
+  input reset, 
+  output [7:0] rgb,
+  output hSync,
+  output vSync,
+  );
+
+
+always @ (clk) begin
+ 
+end     
+   
+
+endmodule // SpaceInvaders
