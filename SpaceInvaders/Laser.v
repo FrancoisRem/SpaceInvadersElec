97b7ce6f141ebff280fac231d78b9c7447f6ac29
garbadrom <33730312+garbadrom@users.noreply.github.com>
2018-10-24 11:18:24 +0200

    finish Laser.v module

--- SpaceInvaders/Laser.v
+++ SpaceInvaders/Laser.v
@@ -24,4 +24,61 @@ parameter SHIP_HEIGHT = 30 ; // Height of the ship
 parameter V_OFFSET = 10 ; // Number of pixels between bottom of the screen and ship
 parameter STEP_MOTION = 1 ; // Number of pixels of vertical laser motion
 
+reg laserAlive;
+assign vgaInLaser = (hPos - xLaser) * (hPos - xLaser) + (vPos - yLaser) * (vPos - yLaser) <= RADIUS * RADIUS;
+
+always @ (clk) begin
+  if (reset) begin
+    laserAlive <= 0;
+  end
+
+  else if (enable) begin
+
+    // LASER ALIVE
+    if (laserAlive) begin
+      if (killingAlien)
+        // we destroy the laser
+        laserAlive <= 0;
+
+      else begin
+        if (yLaser > 0)
+          // update laser position
+          yLaser <= yLaser - STEP_MOTION;
+        else
+          // laser out of screen, so we destroy it
+          laserAlive <= 0;
+      end
+    end
+
+    // LASER NOT ALIVE
+    else begin
+      if (fire)
+        // fire laser
+        laserAlive <= 1;
+    end
+
+    // HANDLE COLOR
+    if (laserAlive & vgaInLaser) 
+      colorLaser = LASER
+    else
+      colorLaser = BACKGROUND
+
+  end
+end
+
+
+// fire the laser or hide it
+always @ (laserAlive) begin
+  if (laserAlive) begin
+    // put the laser at start position
+    xLaser <= gunPosition;
+    yLaser <= SCREEN_HEIGHT - V_OFFSET - SHIP_HEIGHT - RADIUS;
+  end else begin
+    // put the laser at the right bottom of the screen
+    xLaser <= SCREEN_WIDTH - 1;
+    yLaser <= SCREEN_HEIGHT - 1;
+  end
+end
+
+
 endmodule // Laser
