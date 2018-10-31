80e8a0a324bf455d68f10ecce54fa2bc3add3d67
garbadrom <33730312+garbadrom@users.noreply.github.com>
2018-10-31 11:59:52 +0100

    Merge branch 'master' of https://github.com/FrancoisRem/SpaceInvadersElec

--- SpaceInvaders/FinalColor.v
+++ SpaceInvaders/FinalColor.v
@@ -1,21 +1,21 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    10:08:48 10/24/2018 
-// Design Name: 
-// Module Name:    FinalColor 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Company:
+// Engineer:
 //
-// Dependencies: 
+// Create Date:    10:08:48 10/24/2018
+// Design Name:
+// Module Name:    FinalColor
+// Project Name:
+// Target Devices:
+// Tool versions:
+// Description:
 //
-// Revision: 
+// Dependencies:
+//
+// Revision:
 // Revision 0.01 - File Created
-// Additional Comments: 
+// Additional Comments:
 //
 //////////////////////////////////////////////////////////////////////////////////
 module FinalColor(
@@ -32,11 +32,11 @@ parameter NONE = 7;
 reg [2:0] color_reg ;
 assign color = color_reg ;
 
-always @(hPos & vPos) begin
+always @(hPos, vPos) begin
 	if (hPos>640 || vPos>480) begin
 			color_reg = 3'b111 ;
 	end
-	
+
 	else begin
 		color_reg = colorInput ;
 	end
