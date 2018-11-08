9d0fa746c9b76daf5f4eecfe03f8ed47f7433b79
Paul Garnier <44346877+DonsetPG@users.noreply.github.com>
2018-11-08 11:01:38 +0100

    Update Vga.v

--- SpaceInvaders/Vga.v
+++ SpaceInvaders/Vga.v
@@ -1,89 +1,66 @@
+
+
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    09:07:47 10/24/2018 
-// Design Name: 
-// Module Name:    Vga 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module Vga(
+
+module Vga_module(
     input clk,
     input enable,
     input reset,
-    output reg hSync,
-    output reg vSync,
-    output reg [9:0] hPos,
-    output reg [9:0] vPos
+    output hSync,
+    output vSync,
+    output [9:0] hPos,
+    output [9:0] vPos
     );
-	 
-
-localparam HMAX = 800;
-localparam VMAX = 521;
-localparam RESET = 0;
 	
-localparam HS_STA = 640 + 16;              
-localparam HS_END = 640 + 16 + 96;           
-localparam VS_STA = 480 + 11;        
-localparam VS_END = 480 + 11 + 2; 
-	
-always @(posedge clk) begin
-
-if (reset) begin 
-	vSync<=RESET;
-	hSync<=RESET;
-	hPos<=RESET;
-	vPos<=RESET;		
-end 
-
-else begin 
-	if (enable) begin
-		if (hPos == HMAX) begin
-			hPos <= RESET;
-			if (vPos == VMAX) begin
-				vPos <= RESET;
-			end
-			else begin
-				vPos <= vPos + 1;
-			end
-			
-		end
-		else hPos <= hPos + 1;
-			
+	reg signal_h;
+	reg signal_v;
+	reg[9:0] hPos_reg;
+	reg[9:0] vPos_reg;
+	reg[9:0] count;
+	reg[9:0] count_lines;
 		
+	assign hSync = signal_h;
+	assign vSync = signal_v;
+	assign hPos = hPos_reg;
+	assign vPos = vPos_reg;
+	
+	//TimeUnit#(.FREQ_WANTED(25000000),.FREQ_CLK(50000000)) diviseur(.clk(clk), .reset(reset), .pulse(enable)); A Mettre A l'exterieur de ce module
+	
+	always @(posedge clk) begin 
+		if(reset) begin
+			count = 0;
+			signal_h = 0;
+			signal_v = 0;
+			count_lines = 0;
+			hPos_reg = 0;
+			vPos_reg = 0;
+		end
+		else begin
+			if(enable) begin
+				if (count == 143 || count >= 783) hPos_reg = 0;
+				if (count > 143) hPos_reg = hPos_reg + 1;
+				if (count >= 95) signal_h = 1;
+				
+				if (count >= 800) begin
+					signal_h = 0;
+					count = 0;
+					
+					if (count_lines >= 30) vPos_reg = vPos_reg + 1;
+					if (count_lines == 30 || count_lines >= 510) vPos_reg = 0;
+					if (count_lines >= 1) signal_v = 1;
+
+					if(count_lines >= 520) begin
+						count_lines = 0;
+						signal_v = 0;
+					end
+					else count_lines = count_lines + 1;
+					
+				end
+				
+
+				count = count + 1 ;
+			end
+		end
 	end
-	
-	hSync <= ~((hPos >= HS_STA) & (hPos < HS_END));
-	vSync <= ~((vPos >= VS_STA) & (vPos < VS_END));
-	
-	//if(656 <= hPos & hPos < 752) begin
-		//hSync <= 0;
-	//end
-	//else begin
-		//hSync<=1;
-	//end
 
-	//if(490 <= vPos & vPos < 492) begin
-		//vSync <= 0;
-	//end
-	//else begin
-		//vSync<=1;
-	//end	
-end 
-
-end
-
-
-
-endmodule 
+endmodule
