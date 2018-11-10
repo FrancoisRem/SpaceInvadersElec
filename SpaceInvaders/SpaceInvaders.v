3e976dd99c12ea8b3895c5e0a9973eb7c6cb1561
Paul Garnier <44346877+DonsetPG@users.noreply.github.com>
2018-11-10 13:36:09 +0100

    new parameters

--- SpaceInvaders/SpaceInvaders.v
+++ SpaceInvaders/SpaceInvaders.v
@@ -11,6 +11,14 @@ module SpaceInvaders (
   output defeat
   );
 
+
+// Parameters : 
+
+parameter NB_LIN = 2;
+parameter NB_COL = 2;
+parameter NB_ALIENS = NB_LIN*NB_COL;
+
+
 // WIRES
 wire [9:0] hPos;
 wire [9:0] vPos;
@@ -34,9 +42,9 @@ wire enableLaser;
 wire [2:0] motion;
 wire canLeft;
 wire canRight;
-wire [9:0] xAlien;
+wire signed [10:0] xAlien;
 wire [9:0] yAlien;
-wire [35:0] alive;
+wire [NB_ALIENS-1:0] alive;
 
 TimeUnitEnable#(.FREQ_WANTED(25000000)) timeUnitVga(.clk(clk),.reset(reset),
 .enable(1),.pulse(enableVga));
@@ -50,28 +58,36 @@ TimeUnitEnable#(.FREQ_WANTED(300)) timeUnitLaser(.clk(clk),.reset(reset),
 // MODULES
 Button btnLeftModule(.clk(clk), .reset(reset), .pressed(btnLeftInput),
 .pulse(btnLeftWire));
+
+
 Button btnRightModule(.clk(clk), .reset(reset), .pressed(btnRightInput),
 .pulse(btnRightWire));
+
+
 SpaceShip spaceship(.clk(clk),.reset(reset),
 .left(btnLeftWire),.right(btnRightWire),
 .hPos(hPos),.vPos(vPos),.gunPosition(gunPosition),.color(colorSpaceship));
+
 
 Laser laser(.clk(clk),.reset(reset),.enable(enable),
 .fire(fire),.killingAlien(killingAlien),.gunPosition(gunPosition),
 .hPos(hPos),.vPos(vPos),.xLaser(xLaser),.yLaser(yLaser),
 .colorLaser(colorLaser));
 
+
 ZigZagAlien zigZagAlien(.clk(clk),.reset(reset),
 .enable(enableZigZag),.canLeft(canLeft),.canRight(canRight),
 .Motion(motion));
-AliensMotion aliensMotion(
+
+
+AliensMotion#(.NB_LIN(NB_LIN),.NB_COL(NB_COL)) aliensMotion(
   .clk(clk), .reset(reset), .xLaser(xLaser), .yLaser(yLaser), 
   .motion(motion), .hPos(hPos), .vPos(vPos),
   .killingAlien(killingAlien), .canLeft(canLeft), .canRight(canRight),
   .victory(victory), .defeat(defeat),
   .xAlien(xAlien), .yAlien(yAlien), .alive(alive));
   
-ColorAlien colorAlienUnit(.hPos(hPos),.vPos(vPos),.xAlien(xAlien),
+ColorAlien#(.NB_LIN(NB_LIN),.NB_COL(NB_COL)) colorAlienUnit(.hPos(hPos),.vPos(vPos),.xAlien(xAlien),
 .yAlien(yAlien),.alive(alive),.colorAlien(colorAlien));
 
 Vga vga(.clk(clk),.enable(enable),.reset(reset),.hPos(hPos),
@@ -79,7 +95,7 @@ Vga vga(.clk(clk),.enable(enable),.reset(reset),.hPos(hPos),
 FinalColor finalcolor(.colorInput(colorSum),.hPos(hPos),
 .vPos(vPos),.color(colorOutput));
 
-Rgb rgbUnit(.color(colorOutput),.rgb(rgb));
+Rgb rgbUnit(.color(colorOutpust),.rgb(rgb));
 
 always @(posedge clk) begin
 	colorSum = colorLaser + colorAlien + colorSpaceship;
@@ -87,3 +103,4 @@ end
 
 
 endmodule // SpaceInvaders
+
